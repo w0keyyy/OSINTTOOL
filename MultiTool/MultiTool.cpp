4b5a9943bdf455d5e6809f3e9711abf346117c75
w0keyyy <whytokeev@gmail.com>
2025-03-30 17:49:58 +0500

    Update MultiTool.cpp

--- MultiTool/MultiTool.cpp
+++ MultiTool/MultiTool.cpp
@@ -1,62 +1,49 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <windows.h>
 #include <wininet.h>
-
 #pragma comment(lib, "wininet.lib")
-
 using namespace std;
-
 const string PHONE_API_TOKEN = "0b46363467f36e18694fb9425f69788284c4dc10";
 const string IP_API_KEY = "a44ab571c637431aa77efaba7d330cc5";
 const string USER_DATA_FILE = "users.txt";
-
 void printOSINTLogo() {
-    cout << "\n\n";
-    cout << string(30, ' ') << "  ██████  ███████ ██ ███    ██ ████████     ████████  ██████   ██████  ██      \n";
-    cout << string(30, ' ') << " ██       ██      ██ ████   ██    ██           ██    ██    ██ ██    ██ ██      \n";
-    cout << string(30, ' ') << " ██   ███ █████   ██ ██ ██  ██    ██           ██    ██    ██ ██    ██ ██      \n";
-    cout << string(30, ' ') << " ██    ██ ██      ██ ██  ██ ██    ██           ██    ██    ██ ██    ██ ██      \n";
-    cout << string(30, ' ') << "  ██████  ███████ ██ ██   ████    ██           ██     ██████   ██████  ███████ \n";
-    cout << "\n\n";
+    cout << R"(
+   ____   _____ _____ _   _ _______   _______ ____   ____  _
+  / __ \ / ____|_   _| \ | |__   __| |__   __/ __ \ / __ \| |
+ | |  | | (___   | | |  \| |  | |       | | | |  | | |  | | |
+ | |  | |\___ \  | | | . ` |  | |       | | | |  | | |  | | |
+ | |__| |____) |_| |_| |\  |  | |       | | | |__| | |__| | |____
+  \____/|_____/|_____|_| \_|  |_|       |_|  \____/ \____/|______|
+)" << endl;
+
 }
-
-
 //очистка экрана и центрирование текста
 void clearScreen() {
     system("cls");
     printOSINTLogo();  // Вывод ASCII-арта
 }
-
-
-
-
 // Функция для отправки HTTP-запроса
 string sendRequest(const string& url) {
     HINTERNET hInternet = InternetOpenA("MultiTool", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
     if (!hInternet) return "Ошибка InternetOpen";
-
     HINTERNET hConnect = InternetOpenUrlA(hInternet, url.c_str(), NULL, 0, INTERNET_FLAG_RELOAD, 0);
     if (!hConnect) {
         InternetCloseHandle(hInternet);
         return "Ошибка InternetOpenUrl";
     }
-
     char buffer[4096];
     DWORD bytesRead;
     string response;
-
     while (InternetReadFile(hConnect, buffer, sizeof(buffer) - 1, &bytesRead) && bytesRead) {
         buffer[bytesRead] = '\0';
         response += buffer;
     }
-
     InternetCloseHandle(hConnect);
     InternetCloseHandle(hInternet);
     return response;
 }
-
 // тут написал парсер для json вроде ворк хз
 string getValue(const string& json, const string& key) {
     size_t start = json.find("\"" + key + "\":");
@@ -74,7 +61,6 @@ string getValue(const string& json, const string& key) {
         return json.substr(start, end - start);
     }
 }
-
 // тут регатся нужно
 void registerUser() {
     clearScreen();
@@ -83,16 +69,13 @@ void registerUser() {
     cin >> username;
     cout << "Введите пароль: ";
     cin >> password;
-
     ofstream file(USER_DATA_FILE, ios::app);
     file << username << " " << password << endl;
     file.close();
-
     cout << "Регистрация успешна! Нажмите ENTER для продолжения...";
     cin.ignore();
     cin.get();
 }
-
 // тут логинн
 bool loginUser() {
     clearScreen();
@@ -101,7 +84,6 @@ bool loginUser() {
     cin >> username;
     cout << "Введите пароль: ";
     cin >> password;
-
     ifstream file(USER_DATA_FILE);
     while (file >> fileUser >> filePass) {
         if (fileUser == username && filePass == password) {
@@ -110,55 +92,43 @@ bool loginUser() {
         }
     }
     file.close();
-
     cout << "Ошибка входа! Нажмите ENTER для выхода...";
     cin.ignore();
     cin.get();
     return false;
 }
-
 // гетинфо функция для получения инфы про номер тела
 void getPhoneInfo() {
     clearScreen();
     string phone;
     cout << "Введите номер телефона (без +7): ";
     cin >> phone;
-
     string url = "https://api.regius.name/iface/phone-number.php?phone=" + phone + "&token=" + PHONE_API_TOKEN;
     string response = sendRequest(url);
-
     cout << "\nОператор: " << getValue(response, "company") << " (" << getValue(response, "brand") << ")" << endl;
     cout << "Регион: " << getValue(response, "region") << endl;
     cout << "Часовой пояс: UTC" << getValue(response, "utc") << endl;
-
     cout << "\nНажмите ENTER для продолжения...";
     cin.ignore();
     cin.get();
 }
-
 // тут гетайпи для получения данных об ИП
 void getIPInfo() {
     clearScreen();
     string ip;
     cout << "Введите IP-адрес: ";
     cin >> ip;
-
     string url = "https://api.ipgeolocation.io/ipgeo?apiKey=" + IP_API_KEY + "&ip=" + ip;
     string response = sendRequest(url);
-
     cout << "\nСтрана: " << getValue(response, "country_name") << endl;
     cout << "Город: " << getValue(response, "city") << endl;
     cout << "Провайдер: " << getValue(response, "isp") << endl;
-
     cout << "\nНажмите ENTER для продолжения...";
     cin.ignore();
     cin.get();
 }
-
 int main() {
     setlocale(LC_ALL, "RU");
-
-  
     int choice;
     while (true) {
         clearScreen();
@@ -167,12 +137,10 @@ int main() {
         cout << "3. Выход\n";
         cout << "Выберите опцию: ";
         cin >> choice;
-
         if (choice == 1) registerUser();
         else if (choice == 2 && loginUser()) break;
         else if (choice == 3) return 0;
     }
-
     // main menu sigma
     while (true) {
         clearScreen();
@@ -181,7 +149,6 @@ int main() {
         cout << "3. Выйти\n";
         cout << "Выберите опцию: ";
         cin >> choice;
-
         if (choice == 1) getPhoneInfo();
         else if (choice == 2) getIPInfo();
         else if (choice == 3) return 0;
